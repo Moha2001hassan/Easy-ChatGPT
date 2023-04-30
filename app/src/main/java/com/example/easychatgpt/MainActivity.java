1785f0093386c02e4e0131affb212e5f7cec43b1
Mohamed Hassan <mohahassanna933@gmail.com>
2023-04-30 19:50:27 +0200

    adapter & addItemsChat

--- app/src/main/java/com/example/easychatgpt/MainActivity.java
+++ app/src/main/java/com/example/easychatgpt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.easychatgpt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     EditText messageEditText;
     ImageButton sendButton;
     List<Message> messageList;
+    MessageAdapter messageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,33 @@ public class MainActivity extends AppCompatActivity {
         messageEditText = findViewById(R.id.message_edit_text);
         sendButton = findViewById(R.id.send_btn);
 
+        buildRecyclerView();
+
         sendButton.setOnClickListener(v -> {
             String question = messageEditText.getText().toString().trim();
-            Toast.makeText(MainActivity.this, question, Toast.LENGTH_SHORT).show();
+           addToChat(question, Message.SEN_BY_ME);
         });
     }
+
+    private void buildRecyclerView() {
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void addToChat(String message, String sendBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sendBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+
+    }
+
 }
