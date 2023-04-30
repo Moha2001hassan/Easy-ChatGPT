c4ccdec4caf0aad35890063ffb6319e722638a50
Mohamed Hassan <mohahassanna933@gmail.com>
2023-04-30 07:55:26 +0200

    Design and RecyclerView

--- app/src/main/java/com/example/easychatgpt/MainActivity.java
+++ app/src/main/java/com/example/easychatgpt/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.easychatgpt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messageList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        welcomeTextView = findViewById(R.id.welcome_text);
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_btn);
+
+        sendButton.setOnClickListener(v -> {
+            String question = messageEditText.getText().toString().trim();
+            Toast.makeText(MainActivity.this, question, Toast.LENGTH_SHORT).show();
+        });
     }
 }
